a2e8ec4adcc7869c834f74ff3bae614cae242a4e
sihans <66313404+sihans@users.noreply.github.com>
2020-06-02 20:20:43 +0700

    Add files via upload

---
+++ Server transfer image menggunakan flask/app.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask, request, render_template, send_from_directory
+
+__author__ = 'sihans'
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    else:
+        print("Couldn't ceate upload directory: {}".format(target))
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("Accept incoming file:", filename)
+        print ("Save it to:", destination)
+        upload.save(destination)
+
+    return render_template("complete.html", image_name=filename)
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
